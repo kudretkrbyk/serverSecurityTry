89705eabd6f9493cda1f204306525d0a47f62f1d
Kenan Unal <kenan310kenan@gmail.com>
2025-04-14 13:41:04 +0300

    Sequelize for MariaDB

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -1,25 +1,19 @@
-const { pool } = require("../config/db");
+const Kullanici = require('../models/Kullanici');
 
 const getUserByUsername = async (username) => {
-  const conn = await pool.getConnection();
-  const rows = await conn.query(
-    "SELECT * FROM kullanicilar WHERE kullanici_ad = ?",
-    [username]
-  );
-  conn.release();
-  return rows[0];
+  const user = await Kullanici.findOne({ where: { kullanici_ad: username } });
+  return user;
 };
 
 const createUser = async (username, hashedPassword) => {
   console.log("Creating user: model", username, hashedPassword);
-  const conn = await pool.getConnection();
-  await conn.query(
-    "INSERT INTO kullanicilar (kullanici_ad, kullanici_sifre) VALUES (?, ?)",
-    [username, hashedPassword]
-  );
+  await Kullanici.create({
+    kullanici_ad: username,
+    kullanici_sifre: hashedPassword,
+  });
   console.log("burası geldi");
-  conn.release();
 };
+
 module.exports = {
   getUserByUsername,
   createUser,
